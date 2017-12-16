86cfca6f766fcfb96e2307d1327e5d1369b1ec68
oliver <oliversoria@hotmail.com>
2017-12-16 13:47:27 -0600

    ...

--- test/src/main/java/com/project/controller/UsuariosController.java
+++ test/src/main/java/com/project/controller/UsuariosController.java
@@ -3,32 +3,41 @@ package com.project.controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.project.bo.UsuariosBo;
 import com.project.dto.UsuarioDto;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/administrador")
 public class UsuariosController {
 	
 	@Autowired
 	UsuariosBo usuariosBo;
 	
-	@RequestMapping("/usuarios")
-	public ModelAndView welcomeMessage() {
-		// Name of your jsp file as parameter
-		ModelAndView view = new ModelAndView("usuarios");
-		return view;
+	@RequestMapping(value = "/usuarios", method = RequestMethod.GET)
+	public String home(Model model) {
+		return "jsp/usuarios/usuarios";
 	}
 	
 	@RequestMapping(value="/usuarios/consulta", method = RequestMethod.GET)
 	public @ResponseBody ArrayList<UsuarioDto> consultaUsuarios() throws Exception {
 		return usuariosBo.getUsers();
+	}
+	
+	@RequestMapping(value="/usuarios/alta", consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+	public ResponseEntity altaUsuarios(@RequestBody UsuarioDto usuario) throws Exception {
+		//usuariosBo.saveUser()
+		System.out.println(usuario);
+		// return JSONObject.quote("success!");
+		return new ResponseEntity(HttpStatus.OK);
 	}
 }
